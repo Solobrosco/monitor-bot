7330eb2bb3822e95f0e86263ecfd40c8a325739f
Aaron Van Oung <aaron.oung@yahoo.com>
2021-01-25 16:25:45 -0800

    Bot is able to request website by url

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import requests
+import json
+
 print("Hello I am a monitor bot written in python");
+#allows you to access website information
+r = requests.get('https://kith.com/products.json')
+products = json.loads((r.text))['products']
+#print(products)
+
+for product in products:
+    productname = product['title']
+    print(productname)
+
+#print(products[1])
+#used raw_input instead of input 
+text = raw_input("what product are you looking to check: ") 
+
+
+if (productname.find(text)!= -1): 
+    print ("\nProduct "+text+" in stock\n ") 
+else: 
+    print ("Product does not exist")
